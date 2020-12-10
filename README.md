e682cc3660d56477e22021e5f5e38b95e0f74ae9
luis <luisalfonsoredondo@gmail.com>
2020-12-10 14:18:34 +0100

    s

---
+++ README.md
@@ -0,0 +1,4 @@
+# R-Duino-Mcus
+
+Sketchs variados para utilizar en distintos tipos de placas.
+Atmega, Attiny, Esp8266 y python sobre linux armbian utilizando los puertos de gpio. 
---
+++ atmega/blink_mod/Blinkmod.ino
@@ -0,0 +1,13 @@
+int pinled = 13;
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(pinled, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(pinled, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(pinled, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
